cb1fd12389e454c5658229f7c5464e548b99ef91
tuongchaukhai <tuong.chaukhai@gmail.com>
2023-05-01 12:16:18 +0700

    Created a class to customize responses

---
+++ EcomWeb/Models/ApiResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace EcomWeb.Models
+{
+    public class ApiResponse<T>
+    {
+        public int StatusCode { get; set; }
+        public string Message { get; set; }
+        public IEnumerable<T> Data { get; set; }
+    }
+}
