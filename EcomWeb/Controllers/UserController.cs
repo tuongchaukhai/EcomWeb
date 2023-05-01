3683c2f58af9f168bc4e4d2da37cec975b17858c
tuongchaukhai <tuong.chaukhai@gmail.com>
2023-05-01 14:26:29 +0700

    Complete User GetAll()

--- EcomWeb/Controllers/UserController.cs
+++ EcomWeb/Controllers/UserController.cs
@@ -1,12 +1,37 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AutoMapper;
+using EcomWeb.Dtos.User;
+using EcomWeb.Models;
+using EcomWeb.Services;
+using Microsoft.AspNetCore.Mvc;
 
 namespace EcomWeb.Controllers
 {
+    [Route("api/[controller]")]
+    [ApiController]
     public class UserController : Controller
     {
-        public IActionResult Index()
+        private IUserService _userService;
+        private IMapper _mapper;
+
+        public UserController(IUserService userService, IMapper mapper)
         {
-            return View();
+            _userService = userService;
+            _mapper = mapper;
         }
+
+ 
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var users = _userService.GetAll();
+
+            return Ok(new ApiResponse()
+            {
+                StatusCode = 200,
+                Message = "Get all users successfully",
+                Data = _mapper.Map<IEnumerable<UserResultDto>>(users)
+            });
+        }
+
     }
 }
