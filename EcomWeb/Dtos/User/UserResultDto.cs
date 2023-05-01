d0c945153b9cec4efff1a787bedb2554465abd66
tuongchaukhai <tuong.chaukhai@gmail.com>
2023-05-01 14:24:26 +0700

    Added UserResultsDto

---
+++ EcomWeb/Dtos/User/UserResultDto.cs
@@ -0,0 +1,24 @@
+﻿using EcomWeb.Models;
+
+namespace EcomWeb.Dtos.User
+{
+    public class UserResultDto
+    {
+        public int UserId { get; set; }
+
+        public string Email { get; set; } = null!;
+
+        public string Password { get; set; } = null!;
+
+        public bool Active { get; set; }
+
+        public string FullName { get; set; } = null!;
+
+        public DateTime? LastLogin { get; set; }
+
+        public DateTime CreatedDate { get; set; }
+
+        public string RoleName { get; set; }
+
+    }
+}
--- EcomWeb/Program.cs
+++ EcomWeb/Program.cs
@@ -2,20 +2,22 @@ using AutoMapper;
 using EcomWeb.Dtos.User;
 using EcomWeb.Models;
 using EcomWeb.Repository;
+using EcomWeb.Services;
+using Microsoft.EntityFrameworkCore;
+using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
 var connectionString = builder.Configuration.GetConnectionString("MyDbConnection");
-//builder.Services.AddDbContext<MyDbContext>(x => x.UseSqlServer(connectionString));
+builder.Services.AddDbContext<MyDbContext>(x => x.UseSqlServer(connectionString));
 
 // Add services to the container.
-
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddScoped<IUserRepository, UserRepository>();
 
 //AutoMapper
 builder.Services.AddAutoMapper(typeof(Program));
@@ -23,9 +25,17 @@ builder.Services.AddControllersWithViews();
 MapperConfiguration mappingConfig = new MapperConfiguration(mc =>
 {
     mc.CreateMap<UserAddDto, User>();
+    mc.CreateMap<User, UserResultDto>().ForMember(dest => dest.RoleName, opt => opt.MapFrom(src => src.Role.RoleName)); ;
 });
 IMapper mapper = mappingConfig.CreateMapper();
 builder.Services.AddSingleton(mapper);
+
+//builder.Services.AddControllers().AddJsonOptions(x =>
+//                x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
+
+//Scoped
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddScoped<IUserService, UserService>();
 
 
 var app = builder.Build();
