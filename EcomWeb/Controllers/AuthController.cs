1ec85d47294e8e3d12c891e36949394fa921caf1
tuongchaukhai <tuong.chaukhai@gmail.com>
2023-05-08 13:28:43 +0700

    Created login request

---
+++ EcomWeb/Controllers/AuthController.cs
@@ -0,0 +1,59 @@
+﻿using Azure.Core;
+using EcomWeb.Dtos.Auth;
+using EcomWeb.Models;
+using EcomWeb.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+
+namespace EcomWeb.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : Controller
+    {
+        private MyDbContext _context;
+        private TokenService _tokenService;
+        public AuthController(MyDbContext context, TokenService tokenService) {
+            _context = context;
+            _tokenService = tokenService;
+        }
+
+        [HttpPost]
+        public IActionResult Login(LoginDto loginDto)
+        {
+            try
+            {
+                var user = _context.Users.Include(x => x.Role).SingleOrDefault(x => x.Email == loginDto.Email);
+
+                if (user == null)
+                {
+                    throw new Exception("This user doesn't exist");
+                }
+
+                if (!BCrypt.Net.BCrypt.Verify(loginDto.Password, user.Password))
+                {
+                    throw new Exception("Invalid password");
+                }
+
+                var token = _tokenService.GenerateToken(user);
+
+                return Ok(new ApiResponse
+                {
+                    StatusCode = 200,
+                    Message = "Login success",
+                    Data = token
+                });
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new ApiResponse
+                {
+                    StatusCode = 400,
+                    Message = ex.Message
+                });
+            }
+        }
+
+    }
+}
---
+++ EcomWeb/Dtos/Auth/LoginDto.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EcomWeb.Dtos.Auth
+{
+    public class LoginDto
+    {
+        [Required]
+        [EmailAddress(ErrorMessage = "Invalid email address format")]
+        public string Email { get; set; } = string.Empty;
+
+        [Required]
+        [StringLength(16, MinimumLength = 6, ErrorMessage = "Password must be between 6 and 16 characters")]
+        public string Password { get; set; } = string.Empty;
+    }
+}
