db9cf5cea95d87626b401292c051a4f016e20377
tuongchaukhai <tuong.chaukhai@gmail.com>
2023-05-08 09:08:49 +0700

    Added paging, sorting, and paging Product

--- EcomWeb/Repository/IProductRepository.cs
+++ EcomWeb/Repository/IProductRepository.cs
@@ -1,14 +1,17 @@
 ﻿using EcomWeb.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace EcomWeb.Repository
 {
     public interface IProductRepository : IRepositoryBase<Product>
     {
-        new Task<IEnumerable<Product>> GetAll();
+        new Task<IEnumerable<Product>> GetAll(int page = 1, int pageSize = 10);
         Task<Product> GetByTitle(string title);
 
         Task<IEnumerable<Product>> GetByCategory(string categoryName);
 
         Task<IEnumerable<Product>> GetByPriceRange(double min, double max);
+
+        Task<IEnumerable<Product>> Filter([FromQuery] string? title = null, [FromQuery] string? categoryName = null,  double minPrice = 0, double maxPrice = double.MaxValue, string? sortBy = null, int page = 1, int pageSize = 10);
     }
 }
--- EcomWeb/Repository/ProductRepository.cs
+++ EcomWeb/Repository/ProductRepository.cs
@@ -1,4 +1,5 @@
 ﻿using EcomWeb.Models;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace EcomWeb.Repository
@@ -7,9 +8,9 @@ namespace EcomWeb.Repository
     {
         public ProductRepository(MyDbContext context) : base(context) { }
 
-        public override async Task<IEnumerable<Product>> GetAll()
+        public async Task<IEnumerable<Product>> GetAll(int page = 1, int pageSize = 10)
         {
-            return await _context.Products.Include(b => b.Category).ToListAsync();
+            return await _context.Products.Include(b => b.Category).Skip((page - 1) * pageSize).ToListAsync();
         }
 
         public async Task<Product> GetByTitle(string title)
@@ -30,5 +31,44 @@ namespace EcomWeb.Repository
         {
             return await GetByCondition(b => b.Price >= min && b.Price <= max);
         }
+
+        public async Task<IEnumerable<Product>> Filter([FromQuery] string? title = null, string? categoryName = null, double minPrice = 0, double maxPrice = double.MaxValue, string? sortBy = null, int page = 1, int pageSize = 10)
+        {
+            var products = _context.Products.Include(b => b.Category).AsQueryable();
+
+            //Filtering
+            if (!string.IsNullOrEmpty(title))
+            {
+                products = products.Where(b => b.ProductName.Contains(title));
+            }
+            if (!string.IsNullOrEmpty(categoryName))
+            {
+                products = products.Where(b => b.Category.CategoryName == categoryName);
+            }
+
+            products.Where(b => b.Price >= minPrice && b.Price <= maxPrice);
+
+            //Sorting
+            if (!string.IsNullOrEmpty(sortBy))
+            {
+                switch (sortBy)
+                {
+                    case "title_asc": products = products.OrderBy(b => b.ProductName); break;
+                    case "title_desc": products = products.OrderByDescending(b => b.ProductName); break;
+                    case "price_asc": products.OrderBy(b => b.Price); break;
+                    case "price_desc": products.OrderByDescending(b => b.Price); break;
+
+                        //sold value
+
+                }
+            }
+
+            //Paging
+            products = products.Skip((page - 1) * pageSize).Take(pageSize);
+
+            return await products.ToListAsync();
+        }
+
+
     }
 }
