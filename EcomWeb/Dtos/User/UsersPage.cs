ec69a494af40cff9aa04cb2a4facebaafcae3cc5
tuongchaukhai <tuong.chaukhai@gmail.com>
2023-05-09 09:31:38 +0700

    Fixed merge errors

---
+++ EcomWeb/Dtos/User/UsersPage.cs
@@ -0,0 +1,8 @@
+﻿namespace EcomWeb.Dtos.User
+{
+    public class UsersPage
+    {
+        public List<EcomWeb.Models.User> Users { get; set; }
+        public int TotalRecords { get; set; }
+    }
+}
